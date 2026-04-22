de73c5e216b52f390a10a7cf0a17212d968e7513
samanthaa2 <samanthaadams@college.harvard.edu>
2026-04-22 15:01:06 +0000

    Wed, Apr 22, 2026, 11:01 AM -04:00

--- generator.py
+++ generator.py
@@ -3,4 +3,22 @@
 
 ### PART 1: STATIC INPUTS TO CREATE INITIAL PLAN
 
+# This function converts a pace in minutes-per-mile form (like 9.5)
+# into a readable string like 9:30/mile.
+# Useful for readability of printouts
+def format_pace(pace):
+    minutes = int(pace)
+    seconds = round((pace - minutes) * 60)
+
+    # Handle the case where rounding gives 60 seconds.
+    if seconds == 60:
+        minutes += 1
+        seconds = 0
+
+    return f"{minutes}:{seconds:02}/mile"
+
+# this function defines pace target windows for the first week based on the initial information presented
+def init_pace_targets(runnerinfo):
+
+
 
