42ea46ef280de01213f834f1909cb27ef20412a3
samanthaa2 <samanthaadams@college.harvard.edu>
2026-04-22 14:58:00 +0000

    Wed, Apr 22, 2026, 10:58 AM -04:00

--- generator.py
+++ generator.py
@@ -1,10 +1,6 @@
 # A file where the behind the scenes calculations for the plan are conducted
-# version 1 rules
-# 1. Put long run on preferred day.
-# 2. Keep required off days as rest.
-# 3. If running_days >= 5, include one workout day.
-# 4. Do not place workout next to long run if possible.
-# 5. Fill remaining running days with easy runs.
-# 6. Long run starts at longest_recent_run + 1, capped reasonably.
-# 7. Easy pace = average pace + 0.5 to 1.5
-# 8. Long run pace = average pace + 0.75 to 1.75
+
+
+### PART 1: STATIC INPUTS TO CREATE INITIAL PLAN
+
+
